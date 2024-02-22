944404afa6a13f91d320ad74e3e65d87b3c40f85
Rupchand Mahto <mahtorupchand957@gmail.com>
2024-02-22 15:23:15 +0530

    aaghhhhhhhhhhhhhh

--- Basic_implem/database_helper.py
+++ Basic_implem/database_helper.py
@@ -4,6 +4,20 @@ import sqlite3
 
 db_file = 'database.db'
 
+def update_val(id, val):
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    cursor.execute('''
+        UPDATE Data SET value = ? WHERE id = ?
+    ''', (val,id))
+
+def insert_location(location, value):
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO Data (location, value) VALUE (?, ?)
+    ''', (location, value))
+
 def init_db():
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
@@ -15,5 +29,3 @@ def init_db():
             timestamp TIMESTAMP DEFAULT CURRNET_TIMESTAMP
         )
     ''')
-
-    palnsdknd
