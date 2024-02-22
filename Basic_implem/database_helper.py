89bb426f8f16291deabce8e50752fc782bc9d5f7
Rupchand Mahto <mahtorupchand957@gmail.com>
2024-02-22 15:05:55 +0530

    df

--- Basic_implem/database_helper.py
+++ Basic_implem/database_helper.py
@@ -1,11 +1,17 @@
 import asyncio
 import aiosqlite
+import sqlite3
 
-def init_db(connection):
-    connection.execute('''
-        CREATE TABLE IF NOT EXISTS users (
+db_file = 'database.db'
+
+def init_db():
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Data (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            email TEXT NOT NULL UNIQUE
+            location TEXT NOT NULL,
+            value TEXT NOT NULL,
+            timestamp TIMESTAMP DEFAULT CURRNET_TIMESTAMP
         )
     ''')
